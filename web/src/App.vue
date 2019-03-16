1218d413c9e611c13a824d2e232de4a8996f6b4a
kelvingao <89156201@qq.com>
2019-03-16 13:08:37 +0800

    setup the framework

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,49 +1,128 @@
-<template lang="pug">
-  .container
-    h1 Framework JS
-    .row
-      .col-sm-3(v-for="item in items" v-bind:key="item.id")
-        .framework
-        img.framework_image(v-bind:src="item.image")
-        h3.text-center {{ item.title }}
+<template lang='pug'>
+  #app
+    top
+    #header header
+    #nav nav
+    #search search
+    #user user
+    #news news
+    #pic pic
+    #rec rec
+    #sidebar-right sidebar-right
+    #picnews picnews
+    #newslist newslist
+    #link link
+    #footer footer
+   
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import top from '@/components/layout/header.vue'
 
 export default {
   name: 'app',
-  data() {
-    return {
-      items: [
-        {
-          id: 1,
-          title: 'Vue.js',
-          image: ''
-        },
-        {
-          id: 2,
-          title: 'Angular.js',
-          image: ''
-        },
-        {
-          id: 3,
-          title: 'React.js',
-          image: ''
-        },
-        {
-          id: 4,
-          title: 'Aurelia.js',
-          image: ''
-        },
-
-      ]
-    }
+  components: {
+    top
   }
   
 }
 </script>
 
-<style lang="scss">
+<style>
+body, h1, h2, h3, p, form, ul, li, dl, dt, dd {
+  padding: 0;
+  margin: 0;
+}
+body {
+  width: 960px;
+  margin:0 auto;
+  /* border: 1px solid black; */
+  background: #fff;
+}
+#header {
+  height: 80px;
+  background: green;
+  margin: 0 0 10px 0;
+}
 
+#nav {
+  height: 35px;
+  background: navy;
+}
+
+#search {
+  height: 35px;
+  background: maroon;
+  margin: 0 0 10px 0;
+}
+
+#user {
+  width: 270px;
+  height: 400px;
+  background: orange;
+  float: right;
+  margin: 0 0 10px 0;
+}
+
+#news {
+  width: 400px;
+  height: 400px;
+  background: blue;
+  float: right;
+  margin: 0 10px 10px 0;
+}
+
+#pic {
+  width: 270px;
+  height: 195px;
+  background: blue;
+  float: left;
+  margin: 0 0 10px 0;
+}
+
+#rec {
+  width: 270px;
+  height: 195px;
+  background: red;
+  float: left;
+  margin: 0 0 10px 0;
+}
+
+#sidebar-right {
+  width: 270px;
+  height: 835px;
+  background: red;
+  float: right;
+  margin: 0 0 10px 0;
+}
+
+#picnews {
+  width: 680px;
+  height: 200px;
+  background: yellow;
+  float: left;
+  margin: 0 0 10px 0;
+}
+
+#newslist {
+  width: 680px;
+  height: 625px;
+  background: pink;
+  float: left;
+  margin: 0 0 10px 0;
+}
+
+#link {
+  width: 960px;
+  height: 110px;
+  background: orange;
+  float: left;
+  margin: 0 0 10px 0;
+}
+
+#footer {
+  width: 960px;
+  height: 40px;
+  background: blue;
+  float: left;
+}
 </style>
---
+++ web/src/components/layout/header.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+  div
+    #top top
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#top {
+  height: 26px;
+  background: red;
+  margin: 0 0 10px 0;
+}
+</style>
