28c0ff9c844cbbbc392b5e4cf09415236a2c865b
kelvingao <89156201@qq.com>
2019-03-17 00:45:58 +0800

    use bootstrap-vue, setup navbar and login components

--- web/src/components/layout/header.vue
+++ web/src/components/layout/header.vue
@@ -1,6 +1,28 @@
 <template lang='pug'>
-  div
-    #top top
+    #top
+      b-navbar.nav-background(toggleable='lg', type='dark')
+        b-navbar-brand(href='#') NavBar
+        b-navbar-toggle(target='nav_collapse')
+        b-collapse#nav_collapse(is-nav='')
+          b-navbar-nav
+            b-nav-item(href='#') Link
+            b-nav-item(href='#', disabled='') Disabled
+        // Right aligned nav items
+        b-navbar-nav.ml-auto
+          b-nav-form
+            b-form-input.mr-sm-2(size='sm', type='text', placeholder='Search')
+              b-button.my-2.my-sm-0(size='sm', type='submit') Search
+          b-nav-item-dropdown(text='Lang', right='')
+            b-dropdown-item(href='#') EN
+            b-dropdown-item(href='#') ES
+            b-dropdown-item(href='#') RU
+            b-dropdown-item(href='#') FA
+          b-nav-item-dropdown(right='')
+            // Using button-content slot
+            template(slot='button-content')
+              em User
+            b-dropdown-item(href='#') Profile
+            b-dropdown-item(href='#') Signout
 
 </template>
 
@@ -9,9 +31,7 @@ export default {}
 </script>
 
 <style>
-#top {
-  height: 26px;
-  background: red;
-  margin: 0 0 10px 0;
+.nav-background {
+  background: #353535;
 }
 </style>
---
+++ web/src/components/login.vue
@@ -0,0 +1,65 @@
+<template lang='pug'>
+  #login
+    b-container
+      b-row.mt-5(align-h="center")
+        b-col(cols="5")
+          b-card.p-3
+            h3.mb-4 Login
+            b-form(@submit='onSubmit', @reset='onReset', v-if='show')
+              b-form-group#exampleInputGroup1(label='Email address:', label-for='exampleInput1')
+                b-form-input#exampleInput1(type='email', v-model='form.email', required='', placeholder='Enter email')
+              b-form-group#exampleInputGroup2(label='Password:', label-for='exampleInput2')
+                b-form-input#exampleInput2(type='password', v-model='form.password', required='', placeholder='Enter name')
+              b-form-group#exampleGroup4
+                b-form-checkbox-group#exampleChecks(v-model='form.checked')
+                  b-form-checkbox(value='remember') Remember me
+              .d-flex.justify-content-between
+                div
+                  b-button(type='submit', variant='primary') Submit
+                  |&nbsp;
+                  b-button(type='reset', variant='danger') Reset
+                div
+                  a(href='#' v-b-modal.modal1) Forgot Password
+    b-modal#modal1(title="Forgot Password")
+      b-img(src="https://ih0.redbubble.net/image.418429827.6579/ap,550x550,12x16,1,transparent,t.u4.png", fluid alt="Responsive image")
+   
+</template>
+<script>
+
+export default {
+  name: 'login',
+  data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+          checked: []
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        /* Reset our form values */
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        /* Trick to reset/clear native browser form validation state */
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  
+}
+</script>
+
+<style>
+</style>
