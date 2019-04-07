3641bf82b5f9887ef8edec2e73f86b0d50a06dbf
kelvingao <89156201@qq.com>
2019-04-07 22:14:17 +0800

    rewrite login component

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -1,26 +1,18 @@
 <template lang='pug'>
-  //- refs: https://buefy.org/documentation/dropdown
   #login
-    .columns
-      .column.is-one-third.is-offset-one-third.m-t-100
-        .card
-          .card-content
-            h1.title Log In
-            form(@submit="onSubmit", method='POST', role='form')
-              .field
-                label.label(for='email') Email Address
-                p.control
-                  input#email.input(class="", type='text', name='email', placeholder='name@example.com', v-model='form.email')
-                //- p.help.is-danger This email is invalid
-              .field
-                label.label(for='password') Password
-                p.control
-                  input#password.input(class="", type='password', name='password', v-model='form.password')
-                //- p.help.is-danger 
-              b-checkbox.m-t-20( v-model='form.checked') Remember Me
-              button.button.is-success.is-outlined.is-fullwidth.m-t-30 Log In
-        h5.has-text-centered.m-t-20
-          a.is-muted(href="") Forgot Your Password?
+    .column.is-one-third.is-offset-one-third.m-t-100
+      .card.card-content
+        h1.title Log In
+        form(@submit="onSubmit" method="POST" role="form")
+          b-field(label="Email Address")
+            b-input(placeholder="name@example.com" type="email" icon="envelope" required v-model="form.email")
+          b-field(label="Password")
+            b-input(type="password" placeholder="Password regular input" icon="key" v-model="form.password")
+          b-checkbox.m-t-15(v-model="form.checked") Remember Me
+          button.button.is-primary.is-outlined.is-fullwidth.m-t-20 Log In
+
+      h5.has-text-centered.m-t-20
+        router-link.is-muted(to="") Forgot Your Password?
    
 </template>
 <script>
@@ -32,14 +24,12 @@ export default {
       form: {
         email: '',
         password: '',
-        checked: []
+        checked: false
       },
       token:'',
-      show: true
     }
   },
   methods: {
-    //TODO: email and password check
     onSubmit(evt) {
       evt.preventDefault()
       // alert(JSON.stringify(this.form))
@@ -50,21 +40,8 @@ export default {
           this.$router.push('/')
         })
         .catch((e) => {
-          console.error(e)
+          alert(e)
         })
-    },
-    onReset(evt) {
-      evt.preventDefault()
-      /* Reset our form values */
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
     }
   }
   
